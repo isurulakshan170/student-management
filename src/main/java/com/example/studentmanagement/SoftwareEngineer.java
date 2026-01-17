9949b21889b1d4cb844c3537e7196f3a63db82ba
IsuruLakshan170 <lakshanisuru170@gmail.com>
2026-01-17 20:40:09 +0530

    initialize Spring

--- src/main/java/com/example/studentmanagement/SoftwareEngineer.java
+++ src/main/java/com/example/studentmanagement/SoftwareEngineer.java
@@ -1,4 +1,49 @@
 package com.example.studentmanagement;
 
 public class SoftwareEngineer {
+    private Integer id;
+    private String name;
+    private String techStack;
+
+    public SoftwareEngineer() {
+    }
+
+    public SoftwareEngineer(Integer id, String name, String techStack) {
+        this.id = id;
+        this.name = name;
+        this.techStack = techStack;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setTechStack(String techStack) {
+        this.techStack = techStack;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTechStack() {
+        return techStack;
+    }
+
+    @Override
+    public String toString() {
+        return "SoftwareEngineer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", techStack='" + techStack + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/example/studentmanagement/SoftwareEngineerController.java
+++ src/main/java/com/example/studentmanagement/SoftwareEngineerController.java
@@ -1,4 +1,24 @@
 package com.example.studentmanagement;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/software-engineers")
 public class SoftwareEngineerController {
+
+    @GetMapping
+    public List<SoftwareEngineer> getEngineers() {
+      return List.of(
+              new SoftwareEngineer(
+                      123456,
+                      "Alice Johnson",
+                      "tech stack: Java, Spring Boot, React"
+              )
+      );
+    }
 }
